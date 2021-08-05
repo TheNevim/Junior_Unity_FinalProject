7d7f6e2012635092092334916ed3a0dad5ec25b3
Dominik Puk <puk.dominik212@gmail.com>
2021-08-05 12:06:43 +0200

    title-screen
    - animal chooser

---
+++ Programming-Theory-Repo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public string playerAnimal
+    {
+        set
+        {
+            if (value != null && value is string )
+            {
+                playerAnimal = value;
+            }
+        }
+    }
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
---
+++ Programming-Theory-Repo/Assets/Scripts/MenuUIScript.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+
+
+public class MenuUIScript : MonoBehaviour
+{
+    
+    List<String> animalPlayer = new List<string>() { "Dog", "Cat", "Deer" };
+    
+    
+    [SerializeField] private TextMeshProUGUI animalField;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animalField.text = animalPlayer.First();
+        }
+    
+    public void StartGame()
+    {
+        GameManager.Instance.playerAnimal = animalField.text;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        
+    }
+
+    public void ChooseNextAnimal()
+    {
+        var playerIndex = animalPlayer.IndexOf(animalField.text);
+       
+        if ( playerIndex == animalPlayer.Count-1)
+        {
+            animalField.text = animalPlayer.First();
+        }
+        else
+        {
+            animalField.text = animalPlayer.ElementAt(playerIndex+1);
+        }
+        
+    }
+    
+    public void ChoosePrevAnimal()
+    {
+        var playerIndex = animalPlayer.IndexOf(animalField.text);
+        
+        if ( playerIndex == 0)
+        {
+            animalField.text = animalPlayer.Last();
+        }
+        else
+        {
+            animalField.text = animalPlayer.ElementAt(playerIndex-1);
+        }
+    }
+    
+}
